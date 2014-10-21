a22035a70eea3d3faffe4de8491e3c9d8aad39ec
NgaNguyenDuy <kyo1508@gmail.com>
2014-10-22 02:56:37 +0700

    update

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var ds = app.datasources.db,
 app.post('/info', function(req, res){
     // req.body.itb_ajax_form;
     var obj;
-    obj.name = req.body.itb_ajax_form.name;
+    obj.name = req.body.itb_ajax_form.Name;
     obj.email = req.body.itb_ajax_form.email;
     obj.comments = req.body.itb_ajax_form.comments;
 //  var obj2 =  req.body;
