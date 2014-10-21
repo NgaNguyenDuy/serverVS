6ce21aeaf56289acbbd1334fca78b5d95d5880e6
NgaNguyenDuy <kyo1508@gmail.com>
2014-10-22 02:42:09 +0700

    change root route

--- boot/root.js
+++ boot/root.js
@@ -1,8 +1,5 @@
 module.exports = function (server) {
     var router = server.vframe.Router();
     router.get('/', server.vframe.status());
-    router.post('/info', function(req, res){
-	console.log(req.body.itb_ajax_form);
-    });
     server.use(router);
 };
