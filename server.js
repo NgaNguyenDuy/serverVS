53b136c06f48bdca3ed3559ba6e45622085be039
NgaNguyenDuy <kyo1508@gmail.com>
2014-10-22 07:31:34 +0700

    update

--- server.js
+++ server.js
@@ -7,6 +7,23 @@ var app = vf();
 
 app.use(vf.favicon());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+});
+
+
 boot(app, __dirname);
 
 
